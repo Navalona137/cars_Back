10e07b2534783bb9458370b8a3096003487d3199
Navalona137 <118521074+Navalona137@users.noreply.github.com>
2024-01-27 11:11:56 +0100

    deployement 2

--- src/main/java/com/example/voiture/controllers/VoitureController.java
+++ src/main/java/com/example/voiture/controllers/VoitureController.java
@@ -1,6 +1,7 @@
 package com.example.voiture.controllers;
 
 import java.util.List;
+import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.example.voiture.model.Voiture;
+import com.example.voiture.model.TypeCarburant;
+import com.example.voiture.model.Modele;
+import com.example.voiture.model.Users;
 import com.example.voiture.repository.VoitureRepository;
 
 @RestController
@@ -21,4 +25,34 @@ public class VoitureController {
     public List<Voiture> listVoiture() {
         return mp.findAll();
     }
+
+    /*public List<Voiture> listVoitureDetails() {
+        List<Object[]> resultList = mp.listVoitureDetails();
+
+        List<Voiture> voitureList = new ArrayList<>();
+        for (Object[] result : resultList) {
+            Voiture voiture = new Voiture();
+            voiture.setId((Integer) result[0]);
+            voiture.setKilometrage((Integer) result[4]);
+            voiture.setCouleur((String) result[5]);
+            voiture.setPrix((Double) result[6]);
+
+            Users user = new Users();
+            user.setId((Integer) result[1]);
+            
+            Modele modele = new Modele();
+            modele.setNom((String) result[2]);
+
+            TypeCarburant carb = new TypeCarburant();
+            carb.setNom((String) result[3]);
+
+            voiture.setUser(user);
+            voiture.setModele(modele);
+            voiture.setTypeCarburant(carb);
+
+            voitureList.add(voiture);
+        }
+
+        return voitureList;
+    }*/
 }
