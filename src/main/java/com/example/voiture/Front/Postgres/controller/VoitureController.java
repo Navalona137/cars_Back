9434475c9f0ac7f439a96c5ded9b362b3274f0d6
stevenGitHub1357 <stevenratovoson@gmail.com>
2024-01-30 11:07:59 +0300

    Update VoitureController.java

--- src/main/java/com/example/voiture/Front/Postgres/controller/VoitureController.java
+++ src/main/java/com/example/voiture/Front/Postgres/controller/VoitureController.java
@@ -3,15 +3,11 @@ package com.example.voiture.Front.Postgres.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ui.Model;
 
 import com.example.voiture.Front.Postgres.modele.Voiture;
 import com.example.voiture.Front.Postgres.repository.VoitureRepository;
